90c26efa53450d54820d047343cedd9bc7c5b6c6
oid85 <oid85@mail.ru>
2025-05-10 19:10:09 +0700

    Update SmaCrossClosePrice.py

--- SmaCrossClosePrice.py
+++ SmaCrossClosePrice.py
@@ -4,20 +4,20 @@ import backtrader as bt
 
 class SmaCrossClosePrice(bt.Strategy):
     params = (
-        ('SMAPeriod', 26),
-        ('Printing ', False)
+        ('Period', 26),
+        ('EnableLog ', False)
     )
 
-    def log(self, txt, dt=None, doprint=False):
-        if self.p.PrintLog or doprint:
+    def log(self, txt, dt=None, enable_log=False):
+        if self.p.EnableLog or enable_log:
             dt = dt or self.datas[0].datetime.date(0)
             print(f'{dt.isoformat()}, {txt}')
 
     def __init__(self):
         self.DataClose = self.datas[0].close
         self.Order = None
-        self.sma = bt.indicators.SMA(self.datas[0], period=self.params.SMAPeriod)
-        self.brokerStartValue = self.broker.getValue()
+        self.Sma = bt.indicators.SimpleMovingAverage(self.datas[0], period=self.p.SMAPeriod)
+        self.StartMoney = self.broker.getValue()
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -35,7 +35,7 @@ class SmaCrossClosePrice(bt.Strategy):
     def notify_trade(self, trade):
         if not trade.isclosed: # позиция не закрыта
             return
-        self.log(f'Trade PnL={trade.pnl:.2f}, Net={trade.pnlcomm:.2f}')
+        self.log(f'Trade PnL = {trade.pnl:.2f}, Net = {trade.pnlcomm:.2f}')
 
     def next(self):
         self.log(f'Close={self.DataClose[0]:.2f}')
@@ -43,35 +43,34 @@ class SmaCrossClosePrice(bt.Strategy):
             return
 
         if not self.position: # позиции нет
-            signal_buy = self.DataClose[0] > self.sma[0]
+            signal_buy = self.DataClose[0] > self.Sma[0]
             if signal_buy:
                 self.log('Buy')
                 self.Order = self.buy()
         else: # позиция есть
-            signal_sell = self.DataClose[0] < self.sma[0]
+            signal_sell = self.DataClose[0] < self.Sma[0]
             if signal_sell:
                 self.log('Sell')
                 self.Order = self.sell()
 
     def stop(self):
-        self.log(f'SMA({self.p.SMAPeriod}), {(self.broker.getValue() - self.brokerStartValue):.2f}', doprint=True)
+        self.log(f'Money = {(self.broker.getValue() - self.StartMoney):.2f}', enable_log=True)
 
 
 cerebro = bt.Cerebro()
-cerebro.optstrategy(SmaCrossClosePrice, SMAPeriod=range(10, 50))
+cerebro.optstrategy(SmaCrossClosePrice, Period=range(10, 50))
 data = {}
 
 cerebro.adddata(data)
 cerebro.broker.setcash(1000000)
-cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+cerebro.addsizer(bt.sizers.SizerFix, stake=10)
 cerebro.broker.setcommission(commission=0.001)
 cerebro.addanalyzer(bt.analyzers.TradeAnalyzer, _name='TradeAnalyzer')
 print('Результат:')
 results = cerebro.run()
-print('ПУ по сделкам')
 stats = {}
 for result in results:
-    p = result[0].params.SMAPeriod
+    p = result[0].p.Period
     analysis = result[0].analyzers.TradeAnalyzer.get_analysis()
     v = analysis['pnl']['net']['total']
     stats[p] = v
